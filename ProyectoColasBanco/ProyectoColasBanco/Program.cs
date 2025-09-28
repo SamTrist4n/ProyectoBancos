89b778c08910e599fc377dd0aeb361a47a6e4cc0
SamTrist4n <gabriel.inkari@gmail.com>
2025-09-27 22:59:54 -0500

    Update Program.cs

--- ProyectoColasBanco/ProyectoColasBanco/Program.cs
+++ ProyectoColasBanco/ProyectoColasBanco/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,7 +13,7 @@ namespace ProyectoColasBanco
         {
             Console.WriteLine("Bananita Dolphinita, LOL");
             Console.WriteLine("Esto11111111111 es necesariopppppp");
-            Console.WriteLine("XD");
+
         }
     }
 }
