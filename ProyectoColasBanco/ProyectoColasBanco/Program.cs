a6449bc88fc17c7e051bf8bfa831986aea6f4561
SamTrist4n <gabriel.inkari@gmail.com>
2025-09-27 22:09:23 -0500

    no seeee AAAAA

--- ProyectoColasBanco/ProyectoColasBanco/Program.cs
+++ ProyectoColasBanco/ProyectoColasBanco/Program.cs
@@ -11,7 +11,7 @@ namespace ProyectoColasBanco
         static void Main(string[] args)
         {
             Console.WriteLine("Bananita Dolphinita, LOL");
-            Console.WriteLine("Esto es necesario");
+            Console.WriteLine("Esto es necesariopppppp");
         }
     }
 }
